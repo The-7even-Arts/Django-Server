35b76a7bcb868b5426ab046c5de838d22eabb8cf
Tek Solutions <102222252+The-7even-Arts@users.noreply.github.com>
2023-03-04 23:49:06 +0000

    Added Utility Files

---
+++ frontend/utils.py
@@ -0,0 +1,14 @@
+from django.http import HttpRequest, HttpResponse
+
+def get_client_ip(request: HttpRequest) -> str:
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def user_ip(request):
+    ip = get_client_ip(request)
+    return str(ip)
--- frontend/views.py
+++ frontend/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from .utils import get_client_ip
 # Create your views here.
 
 def homepage_view(request):
+    user_ip = get_client_ip(request)
+    
     context = {}
     if not len(Article.objects.all()) > 0:
         pass
         # return HttpResponse("<h4> No Articles Yet </h4>")
     
     articles = Article.objects.all().order_by("-id")
-    context = {'articles':articles}    
+    context = {'articles':articles, 'user_ip':user_ip}    
     return render(request, 'homepage.html', context)
